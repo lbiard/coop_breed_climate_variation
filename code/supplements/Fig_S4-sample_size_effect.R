ad57285378d0efcc9d335f7d4b8747c8e18fbc90
Louis Bliard <69518297+lbiard@users.noreply.github.com>
2026-08-27 13:50:02 +0200

    Update Fig_S4-sample_size_effect.R

--- code/supplements/Fig_S4-sample_size_effect.R
+++ code/supplements/Fig_S4-sample_size_effect.R
@@ -31,7 +31,7 @@ plot_year_temp <- ggplot(population_sensitivity_temp, aes(x=sample_size, y=abs_s
 plot_year_rain <- ggplot(population_sensitivity_rain, aes(x=sample_size, y=abs_sens))+
   geom_jitter(alpha = 0.2, width = 0.2, height = 0)+
   xlab("Number of years")+
-  ylab(expression(paste("Absolute rainfall sensitivity (log ", lambda, " change per 100mm)")))+
+  ylab(expression(paste("Absolute precipitation sensitivity (log ", lambda, " change per 100mm)")))+
   theme_bw() +
   theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank())+
   theme(plot.title = element_text(size=10, hjust = 0.5),
